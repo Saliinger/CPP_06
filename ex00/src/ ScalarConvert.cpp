84a9936bc2155553cc71183ad57193412f53ec3d
Saliinger <alexisnoukan@gmail.com>
2025-05-23 18:15:17 +0200

    add all convertion handler

--- ex00/src/ ScalarConvert.cpp
+++ ex00/src/ ScalarConvert.cpp
@@ -10,9 +10,11 @@ ScalarConvert &ScalarConvert::operator=(const ScalarConvert &src) {}
 
 ScalarConvert::~ScalarConvert() {}
 
-// function
+// helper function
+
+// get the type of the scalar variable
 static int getType(const std::string &s) {
-  if (s.length() == 1)
+  if (s.length() == 1 && !std::isdigit(s[0]))
     return 0;
   else if (1 == 0) {
     return 1;
@@ -24,15 +26,97 @@ static int getType(const std::string &s) {
   return -1;
 }
 
-static void charIn(const std::string &s) {}
+// converters
+static void charIn(const std::string &s) {
+  std::vector<std::string> res;
 
-static void intIn(const std::string &s) {}
+  char c = s[0];  // base
+  int i = static_cast<int>(c);
+  float f = static_cast<float>(c);
+  double d = static_cast<double>(c);
 
-static void floatIn(const std::string &s) {}
+  res.push_back("char: " + std::to_string(c));
+  res.push_back("int: " + std::to_string(i));
+  res.push_back("float: " + std::to_string(f) + "f");
+  res.push_back("double: " + std::to_string(d));
 
-static void doubleIn(const std::string &s) {}
+  for (std::string n : res) std::cout << n << std::endl;
+}
 
+static void intIn(const std::string &s) {
+  std::vector<std::string> res;
+
+  int i = std::stoi(s);  // base
+  char c = static_cast<char>(i);
+  float f = static_cast<float>(i);
+  double d = static_cast<double>(i);
+
+  res.push_back("char: " + std::to_string(c));
+  res.push_back("int: " + std::to_string(i));
+  res.push_back("float: " + std::to_string(f) + "f");
+  res.push_back("double: " + std::to_string(d));
+
+  for (std::string n : res) std::cout << n << std::endl;
+}
+
+static void floatIn(const std::string &s) {
+  std::vector<std::string> res;
+
+  float f = std::stof(s);  // base
+  char c = static_cast<char>(f);
+  int i = static_cast<int>(f);
+  double d = static_cast<double>(f);
+
+  res.push_back("char: " + std::to_string(c));
+  res.push_back("int: " + std::to_string(i));
+  res.push_back("float: " + std::to_string(f) + "f");
+  res.push_back("double: " + std::to_string(d));
+
+  for (std::string n : res) std::cout << n << std::endl;
+}
+
+static void doubleIn(const std::string &s) {
+  std::vector<std::string> res;
+
+  double d = std::stod(s);  // base
+  char c = static_cast<char>(d);
+  int i = static_cast<int>(d);
+  float f = static_cast<float>(d);
+
+  res.push_back("char: " + std::to_string(c));
+  res.push_back("int: " + std::to_string(i));
+  res.push_back("float: " + std::to_string(f) + "f");
+  res.push_back("double: " + std::to_string(d));
+
+  for (std::string n : res) std::cout << n << std::endl;
+}
+
+// main function
 void ScalarConvert::convert(const std::string &s) {
+  // special case
+  if (s == "nan" || s == "nanf") {
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: nanf" << std::endl;
+    std::cout << "double: nan" << std::endl;
+    return;
+  }
+  if (s == "+inf" || s == "+inff" || s == "inf" || s == "inff") {
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: inff" << std::endl;
+    std::cout << "double: inf" << std::endl;
+    return;
+  }
+  if (s == "-inf" || s == "-inff") {
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: -inff" << std::endl;
+    std::cout << "double: -inf" << std::endl;
+    return;
+  }
+
+  // other case
   int type = getType(s);
 
   void (*fill[4])(const std::string &) = {
